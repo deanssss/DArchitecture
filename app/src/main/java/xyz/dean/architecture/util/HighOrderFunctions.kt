c5b2c4f702e779413bb8e49eb02e391a01e9a2f7
ZhaoDi <di.zhao@fenrir-inc.com.cn>
2021-02-24 14:14:50 +0800

    Fixed a bug.

--- app/src/main/java/xyz/dean/architecture/util/HighOrderFunctions.kt
+++ app/src/main/java/xyz/dean/architecture/util/HighOrderFunctions.kt
@@ -1,9 +1,11 @@
 package xyz.dean.architecture.util
 
-inline fun <T> T.onNull(action: () -> Unit) {
+inline fun <T> T.onNull(action: () -> Unit): T {
     this ?: action()
+    return this
 }
 
-inline fun <T> T.noNull(action: (T) -> Unit) {
+inline fun <T> T.noNull(action: (T) -> Unit): T {
     if (this != null) action(this)
+    return this
 }
